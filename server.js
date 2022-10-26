e06b7b48e98c5845df27c7ef42b268e9082ec173
fin-ch <woody101@snu.ac.kr>
2022-10-27 05:29:00 +0900

    ..

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const path = require("path");
 const axios = require("axios");
