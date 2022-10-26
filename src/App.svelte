68737a709aa329c14f5347cc5fef380e90307682
fin-ch <woody101@snu.ac.kr>
2022-10-27 03:43:01 +0900

    ..

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let name;
+
+	import Map from './Map.svelte';
+	export let ready;
+</script>
+
+<svelte:head>
+	<script defer async
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7Z037LAtqUt9IU3NzFN7Mm43gzAT3Wrg&callback=initMap">
+	</script>
+</svelte:head>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+</main>
+
+{ #if ready }
+<Map></Map>
+{ /if }
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+	:global(body) {
+	padding: 0;
+	}
+</style>
