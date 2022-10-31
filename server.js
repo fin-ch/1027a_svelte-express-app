b5c8dbad557214b99c7db2231f037865fe8753c1
fin-ch <woody101@snu.ac.kr>
2022-11-01 07:11:57 +0900

    ..

--- server.js
+++ server.js
@@ -4,12 +4,27 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 const path = require("path");
 const axios = require("axios");
+const fs = require("fs");
 
 app.use(cors());
+app.use(express.json());
 
 app.use(express.static("public"));
 
 app.post("/", (req, res) => {
+    var temp;
+    fs.readFile("./db.json", function (err, data) {
+        if (err) throw err;
+        console.log("data1");
+        console.log(JSON.parse(data));
+        temp = JSON.parse(data);
+        console.log("data3");
+        console.log(temp.device);
+        temp.device[0].time = req.body.time;
+        fs.writeFile("./db.json", JSON.stringify(temp), function (err) {
+            if (err) throw err;
+        });
+    });
     res.end();
 });
 
