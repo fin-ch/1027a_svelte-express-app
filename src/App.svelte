05589b4f041c1c5cc3feef3c09a03d6f66f8cae8
fin-ch <woody101@snu.ac.kr>
2022-11-01 21:00:26 +0900

    ..

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,10 @@
 <script>
+	import JSONTree from 'svelte-json-tree';
+	import item from '../db.json';
+	// import {items} from './data.json';
 	export let name;
+	
+	console.log(item);
 </script>
 
 <svelte:head>
@@ -9,9 +14,17 @@
 </svelte:head>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h2>Hello {name}!</h2>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 
+	<h1>Start Point</h1>
+	<p>lat: {item.config[0].location.lat}, long: {item.config[0].location.long}</p>
+	<h1>End Point</h1>
+	<p>lat: {item.config[10].location.lat}, long: {item.config[10].location.long}</p>
+	<h1>Device</h1>
+	<p>date: {item.device[0].date.d}.{item.device[0].date.m}.{item.device[0].date.y}</p>
+	<p>time: {item.device[0].time.h}.{item.device[0].time.m}.{item.device[0].time.s}.{item.device[0].time.ms}</p>
+	<p>lat: {item.device[0].location.lat}, long: {item.device[0].location.long}</p>
 </main>
 
 <style>
@@ -23,6 +36,13 @@
 	}
 
 	h1 {
+		color: #7856ff;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 50;
+	}
+
+	h2 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
